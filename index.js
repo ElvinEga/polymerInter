fbaa5b8eb14b83d8e2d8f5979f9d1a93875dc621
Elvin Ega <proega3@gmail.com>
2017-10-11 21:16:19 +0300

    node expressjs

---
+++ index.js
@@ -0,0 +1,17 @@
+const express = require('express');
+
+
+//set up express app
+const app = express();
+app.get('/',function(req,res){
+    res.sendFile(__dirname+'/index.html');
+  
+  });
+  app.use('/src',express.static('src'));
+  app.use('/bower_components',express.static('bower_components'));
+  app.use('/images',express.static('images'));
+
+//listen for requests
+app.listen(process.env.port||4000, function(){
+  console.log('now listening for requests');
+});
